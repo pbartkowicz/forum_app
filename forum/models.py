a6827e1fe4cd4552b935cea1318189344943943e
pbartkowicz <pat.bartkowicz@gmail.com>
2018-06-18 11:36:28 +0200

    fixed adding dates

--- forum/models.py
+++ forum/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from datetime import datetime
 from django.contrib.auth.models import User
 
 
 class Topic(models.Model):
     name = models.CharField(max_length=50)
-    published_date = models.DateTimeField('published date', default=datetime.now())
+    published_date = models.DateTimeField('published date', auto_now_add=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -16,7 +15,7 @@ class Post(models.Model):
     post_text = models.TextField(max_length=800)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    published_date = models.DateTimeField('published date', default=datetime.now())
+    published_date = models.DateTimeField('published date', auto_now_add=True, blank=True)
 
     def __str__(self):
         return self.post_text
@@ -26,7 +25,7 @@ class Comment(models.Model):
     comment_text = models.TextField(max_length=800)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    published_date = models.DateTimeField('published date', default=datetime.now())
+    published_date = models.DateTimeField('published date', auto_now_add=True, blank=True)
 
     def __str__(self):
         return self.comment_text
